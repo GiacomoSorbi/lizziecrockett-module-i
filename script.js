e345193cf55ecada15cf7d5d166eb74da9ed5a3d
Lizzie <elizabethsiancrockett@gmail.com>
2020-04-06 23:32:57 +0100

    only prettifying left to do!

--- script.js
+++ script.js
@@ -10,38 +10,33 @@ else if (hrs >= 17 && hrs <= 24)
 document.getElementById('guestGreeting').innerHTML =
     `${greet}, jam enthusiast. <br/> Here's what's currently in the pantry. </br> Orders are first come first served, </br> so pick up the phone!`;
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    
-
-let slide_index = 1;
-displaySlides(slide_index);
-function nextSlide(n) {
-    displaySlides(slide_index += n);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
+
 function currentSlide(n) {
-    displaySlides(slide_index = n);
+    showSlides(slideIndex = n);
 }
-function displaySlides(n) {
+
+function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("showSlide");
-    if (n > slides.length) { slide_index = 1 }
-    if (n < 1) { slide_index = slides.length }
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slide_index - 1].style.display = "block";
-}
-
-
-
-function expandMenu() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
+
+//this denotes a function requiring back-end functionality not yet built
 dummyFunction()
-  //this denotes a function requiring back-end functionality not yet built
