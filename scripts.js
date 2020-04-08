31468aacc5e973c25f4138ecff2f2b001e0ab9b5
Lizzie <elizabethsiancrockett@gmail.com>
2020-04-08 18:47:39 +0100

    minor changes and new scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+//nav menu code
+// in default (mobile) setting, clicking the bars icon changes class of nav items to "topnav responsive" so that they display appropriately 
+function expandMenu() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
