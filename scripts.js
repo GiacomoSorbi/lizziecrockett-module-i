46c68e1fd46a75da6278b259a7c5509062575ba1
Lizzie <elizabethsiancrockett@gmail.com>
2020-04-16 11:01:39 +0100

    lets and consts refined

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 //nav menu code
 // in default (mobile) setting, clicking the bars icon changes class of nav items to "topnav responsive" so that they display appropriately 
 function expandMenu() {
-    let x = document.getElementById("myTopnav");
+    const x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
